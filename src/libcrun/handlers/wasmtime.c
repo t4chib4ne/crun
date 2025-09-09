f51c40476468bd6666da573145ad91fc0f5997ce
t4chib4ne <t4chib4ne@mailbox.org>
2025-09-09 21:50:24 +0200

    Some fooling around

--- src/libcrun/handlers/wasmtime.c
+++ src/libcrun/handlers/wasmtime.c
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with crun.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <string.h>
 #define _GNU_SOURCE
 
 #include <config.h>
@@ -41,6 +42,13 @@
 #  include <wasm.h>
 #  include <wasi.h>
 #  include <wasmtime.h>
+#  include <wasmtime/component/component.h>
+#  include <wasmtime/component/func.h>
+#  include <wasmtime/component/instance.h>
+#  include <wasmtime/component/linker.h>
+#  include <wasmtime/error.h>
+#  include <wasmtime/store.h>
+#  include <wasmtime/wasip2.h>
 #endif
 
 #if HAVE_DLOPEN && HAVE_WASMTIME
@@ -48,6 +56,8 @@ static int
 libwasmtime_exec (void *cookie, libcrun_container_t *container arg_unused,
                   const char *pathname, char *const argv[])
 {
+  fprintf (stderr, "geladen!\n");
+
   size_t args_size = 0;
   char *const *arg;
   wasm_byte_vec_t error_message;
@@ -187,66 +197,212 @@ libwasmtime_exec (void *cookie, libcrun_container_t *container arg_unused,
   // Compile wasm modules
   wasmtime_module_t *module = NULL;
   err = wasmtime_module_new (engine, (uint8_t *) wasm.data, wasm.size, &module);
-  if (! module)
+  if (module)
+    {
+      wasm_byte_vec_delete (&wasm);
+
+      // Init WASI program
+      wasi_config_t *wasi_config = wasi_config_new ("crun_wasi_program");
+      assert (wasi_config);
+
+      // Calculate argc for `wasi_config_set_argv`
+      for (arg = argv; *arg != NULL; ++arg)
+        args_size++;
+
+      wasi_config_set_argv (wasi_config, args_size, (const char **) argv);
+      wasi_config_inherit_env (wasi_config);
+      wasi_config_inherit_stdin (wasi_config);
+      wasi_config_inherit_stdout (wasi_config);
+      wasi_config_inherit_stderr (wasi_config);
+      wasi_config_preopen_dir (wasi_config, ".", ".");
+      wasm_trap_t *trap = NULL;
+      err = wasmtime_context_set_wasi (context, wasi_config);
+      if (err != NULL)
+        {
+          wasmtime_error_message (err, &error_message);
+          wasmtime_error_delete (err);
+          error (EXIT_FAILURE, 0, "failed to instantiate WASI: %.*s", (int) error_message.size, error_message.data);
+        }
+
+      // Init module
+      err = wasmtime_linker_module (linker, context, "", 0, module);
+      if (err != NULL)
+        {
+          wasmtime_error_message (err, &error_message);
+          wasmtime_error_delete (err);
+          error (EXIT_FAILURE, 0, "failed to instantiate module: %.*s", (int) error_message.size, error_message.data);
+        }
+
+      // Actually run our .wasm
+      wasmtime_func_t func;
+      err = wasmtime_linker_get_default (linker, context, "", 0, &func);
+      if (err != NULL)
+        {
+          wasmtime_error_message (err, &error_message);
+          wasmtime_error_delete (err);
+          error (EXIT_FAILURE, 0, "failed to locate default export for module %.*s", (int) error_message.size, error_message.data);
+        }
+
+      err = wasmtime_func_call (context, &func, NULL, 0, NULL, 0, &trap);
+      if (err != NULL || trap != NULL)
+        {
+          wasmtime_error_message (err, &error_message);
+          wasmtime_error_delete (err);
+          error (EXIT_FAILURE, 0, "error calling default export: %.*s", (int) error_message.size, error_message.data);
+        }
+
+      // Clean everything
+      wasmtime_module_delete (module);
+      wasmtime_store_delete (store);
+      wasm_engine_delete (engine);
+
+      exit (EXIT_SUCCESS);
+    }
+
+  // Reset wasmtime context
+  wasmtime_store_delete (store);
+  wasm_engine_delete (engine);
+  engine = wasm_engine_new ();
+  assert (engine != NULL);
+  store = wasmtime_store_new (engine, NULL, NULL);
+  assert (store != NULL);
+  context = wasmtime_store_context (store);
+
+  // Try using Component Model
+  wasmtime_error_t *(*wasmtime_component_new) (const wasm_engine_t *engine, const uint8_t *buf, size_t len, wasmtime_component_t **component_out);
+  wasmtime_wasip2_config_t *(*wasmtime_wasip2_config_new) (void);
+  void (*wasmtime_wasip2_config_inherit_stdin) (wasmtime_wasip2_config_t *config);
+  void (*wasmtime_wasip2_config_inherit_stdout) (wasmtime_wasip2_config_t *config);
+  void (*wasmtime_wasip2_config_inherit_stderr) (wasmtime_wasip2_config_t *config);
+  void (*wasmtime_context_set_wasip2) (wasmtime_context_t *context, wasmtime_wasip2_config_t *config);
+  wasmtime_component_linker_t *(*wasmtime_component_linker_new) (wasm_engine_t *engine);
+  wasmtime_error_t *(*wasmtime_component_linker_add_wasip2) (wasmtime_component_linker_t *linker);
+  wasmtime_error_t *(*wasmtime_component_linker_instantiate) (
+      const wasmtime_component_linker_t *linker,
+      wasmtime_context_t *context,
+      const wasmtime_component_t *component,
+      wasmtime_component_instance_t **instance_out);
+  wasmtime_component_export_index_t *(*wasmtime_component_get_export_index) (
+      wasmtime_component_t *component,
+      wasmtime_component_export_index_t *index,
+      const char *name,
+      size_t name_len);
+  wasmtime_component_export_index_t *(*wasmtime_component_instance_get_export_index) (
+      const wasmtime_component_instance_t *instance,
+      wasmtime_context_t *context,
+      const wasmtime_component_export_index_t *instance_export_index,
+      const char *name,
+      size_t name_len);
+  bool (*wasmtime_component_instance_get_func) (
+      const wasmtime_component_instance_t *instance,
+      wasmtime_context_t *context,
+      const wasmtime_component_export_index_t *export_index,
+      wasmtime_component_func_t *func_out);
+  wasmtime_error_t *(*wasmtime_component_func_call) (
+      const wasmtime_component_func_t *func,
+      wasmtime_context_t *context,
+      const wasmtime_component_val_t *args,
+      size_t args_size,
+      wasmtime_component_val_t *results,
+      size_t results_size);
+  void (*wasmtime_component_export_index_delete) (wasmtime_component_export_index_t *export_index);
+  void (*wasmtime_component_delete) (wasmtime_component_t *c);
+  void (*wasmtime_component_linker_delete) (wasmtime_component_linker_t *linker);
+
+  wasmtime_component_new = dlsym (cookie, "wasmtime_component_new");
+  wasmtime_wasip2_config_new = dlsym (cookie, "wasmtime_wasip2_config_new");
+  wasmtime_wasip2_config_inherit_stdin = dlsym (cookie, "wasmtime_wasip2_config_inherit_stdin");
+  wasmtime_wasip2_config_inherit_stdout = dlsym (cookie, "wasmtime_wasip2_config_inherit_stdout");
+  wasmtime_wasip2_config_inherit_stderr = dlsym (cookie, "wasmtime_wasip2_config_inherit_stderr");
+  wasmtime_context_set_wasip2 = dlsym (cookie, "wasmtime_context_set_wasip2");
+  wasmtime_component_linker_new = dlsym (cookie, "wasmtime_component_linker_new");
+  wasmtime_component_linker_add_wasip2 = dlsym (cookie, "wasmtime_component_linker_add_wasip2");
+  wasmtime_component_linker_instantiate = dlsym (cookie, "wasmtime_component_linker_instantiate");
+  wasmtime_component_get_export_index = dlsym (cookie, "wasmtime_component_get_export_index");
+  wasmtime_component_instance_get_export_index = dlsym (cookie, "wasmtime_component_instance_get_export_index");
+  wasmtime_component_instance_get_func = dlsym (cookie, "wasmtime_component_instance_get_func");
+  wasmtime_component_func_call = dlsym (cookie, "wasmtime_component_func_call");
+  wasmtime_component_export_index_delete = dlsym (cookie, "wasmtime_component_export_index_delete");
+  wasmtime_component_delete = dlsym (cookie, "wasmtime_component_delete");
+  wasmtime_component_linker_delete = dlsym (cookie, "wasmtime_component_linker_delete");
+
+  if (wasmtime_component_new == NULL || wasmtime_wasip2_config_new == NULL || wasmtime_wasip2_config_inherit_stdin == NULL
+      || wasmtime_wasip2_config_inherit_stdout == NULL || wasmtime_wasip2_config_inherit_stderr == NULL
+      || wasmtime_context_set_wasip2 == NULL || wasmtime_component_linker_new == NULL || wasmtime_component_linker_add_wasip2 == NULL
+      || wasmtime_component_linker_instantiate == NULL || wasmtime_component_get_export_index == NULL
+      || wasmtime_component_instance_get_func == NULL || wasmtime_component_func_call == NULL || wasmtime_component_export_index_delete == NULL
+      || wasmtime_component_delete == NULL || wasmtime_component_linker_delete == NULL)
+    error (EXIT_FAILURE, 0, "could not find symbol in `libwasmtime.so");
+
+  fprintf (stderr, "Loading component ...\n");
+  wasmtime_component_t *component = NULL;
+  err = wasmtime_component_new (engine, (uint8_t *) wasm.data, wasm.size, &component);
+  if (! component || err != NULL)
     {
       wasmtime_error_message (err, &error_message);
       wasmtime_error_delete (err);
-      error (EXIT_FAILURE, 0, "failed to compile module: %.*s", (int) error_message.size, error_message.data);
+      error (EXIT_FAILURE, 0, "failed to compile component: %.*s", (int) error_message.size, error_message.data);
     }
+
   wasm_byte_vec_delete (&wasm);
 
-  // Init WASI program
-  wasi_config_t *wasi_config = wasi_config_new ("crun_wasi_program");
-  assert (wasi_config);
+  fprintf (stderr, "creating config\n");
+  wasmtime_wasip2_config_t *wasi_config = wasmtime_wasip2_config_new ();
+  assert (wasi_config != NULL);
 
-  // Calculate argc for `wasi_config_set_argv`
-  for (arg = argv; *arg != NULL; ++arg)
-    args_size++;
+  wasmtime_wasip2_config_inherit_stdin (wasi_config);
+  wasmtime_wasip2_config_inherit_stdout (wasi_config);
+  wasmtime_wasip2_config_inherit_stderr (wasi_config);
+  // TODO: argv
+  wasmtime_context_set_wasip2 (context, wasi_config);
 
-  wasi_config_set_argv (wasi_config, args_size, (const char **) argv);
-  wasi_config_inherit_env (wasi_config);
-  wasi_config_inherit_stdin (wasi_config);
-  wasi_config_inherit_stdout (wasi_config);
-  wasi_config_inherit_stderr (wasi_config);
-  wasi_config_preopen_dir (wasi_config, ".", ".");
-  wasm_trap_t *trap = NULL;
-  err = wasmtime_context_set_wasi (context, wasi_config);
+  fprintf (stderr, "creating linker\n");
+  wasmtime_component_linker_t *comp_linker = wasmtime_component_linker_new (engine);
+  err = wasmtime_component_linker_add_wasip2 (comp_linker);
   if (err != NULL)
     {
       wasmtime_error_message (err, &error_message);
       wasmtime_error_delete (err);
-      error (EXIT_FAILURE, 0, "failed to instantiate WASI: %.*s", (int) error_message.size, error_message.data);
+      error (EXIT_FAILURE, 0, "failed to add WASIp2 to component: %.*s", (int) error_message.size, error_message.data);
     }
 
-  // Init module
-  err = wasmtime_linker_module (linker, context, "", 0, module);
+  // Init Component with linker
+  wasmtime_component_instance_t *component_inst = NULL;
+  fprintf (stderr, "init component\n");
+  err = wasmtime_component_linker_instantiate (comp_linker, context, component, &component_inst);
   if (err != NULL)
     {
       wasmtime_error_message (err, &error_message);
       wasmtime_error_delete (err);
-      error (EXIT_FAILURE, 0, "failed to instantiate module: %.*s", (int) error_message.size, error_message.data);
+      error (EXIT_FAILURE, 0, "failed to instantiate component: %.*s", (int) error_message.size, error_message.data);
     }
+  assert (component_inst);
 
-  // Actually run our .wasm
-  wasmtime_func_t func;
-  err = wasmtime_linker_get_default (linker, context, "", 0, &func);
+  // Get the run func
+  fprintf (stderr, "get func index?!\n");
+  wasmtime_component_export_index_t *run_index = wasmtime_component_instance_get_export_index (component_inst, context, NULL, "wasi:cli/run@0.2.0", strlen ("wasi:cli/run@0.2.0"));
+  assert (run_index);
+
+  fprintf (stderr, "get func itself\n");
+  wasmtime_component_func_t *run_func = NULL;
+  bool found_run = wasmtime_component_instance_get_func (component_inst, context, run_index, run_func);
+  assert (found_run);
+
+  // Call the func
+  fprintf (stderr, "call func\n");
+  err = wasmtime_component_func_call (run_func, context, NULL, 0, NULL, 0);
   if (err != NULL)
     {
       wasmtime_error_message (err, &error_message);
       wasmtime_error_delete (err);
-      error (EXIT_FAILURE, 0, "failed to locate default export for module %.*s", (int) error_message.size, error_message.data);
+      error (EXIT_FAILURE, 0, "could not call run export: %.*s", (int) error_message.size, error_message.data);
     }
 
-  err = wasmtime_func_call (context, &func, NULL, 0, NULL, 0, &trap);
-  if (err != NULL || trap != NULL)
-    {
-      wasmtime_error_message (err, &error_message);
-      wasmtime_error_delete (err);
-      error (EXIT_FAILURE, 0, "error calling default export: %.*s", (int) error_message.size, error_message.data);
-    }
-
-  // Clean everything
-  wasmtime_module_delete (module);
+  // Cleanup
+  fprintf (stderr, "cleanup\n");
+  wasmtime_component_export_index_delete (run_index);
+  wasmtime_component_delete (component);
+  wasmtime_component_linker_delete (comp_linker);
   wasmtime_store_delete (store);
   wasm_engine_delete (engine);
 
@@ -289,7 +445,7 @@ libwasmtime_can_handle_container (libcrun_container_t *container, libcrun_error_
 struct custom_handler_s handler_wasmtime = {
   .name = "wasmtime",
   .alias = "wasm",
-  .feature_string = "WASM:wasmtime",
+  .feature_string = "WASM:wasmtime:t4chib4ne",
   .load = libwasmtime_load,
   .unload = libwasmtime_unload,
   .run_func = libwasmtime_exec,
